5c53f1874345348fe54c2c75c6ba170044591a2a
Danya Generalov <danya@danya02.ru>
2020-12-03 14:11:32 +0300

    Adding cookie sending to server.

--- cookie-extension/send_cookies.js
+++ cookie-extension/send_cookies.js
@@ -1,7 +1,26 @@
+const MY_SECRET = "(replace with value from your database)";
+
 async function sendCookies(details) {
   console.log("REQUEST CAPTURED", details);
   let cookies = await browser.cookies.getAll({'domain': 'dnevnik.mos.ru'});
   console.log("MY COOKIES", cookies);
+  var cookies_formatted = {};
+
+  cookies.forEach((item) => {
+    cookies_formatted[ item.name ] = item.value;
+  });
+
+
+  var xhr = new XMLHttpRequest();
+//  var url = "https://dnevnik-automate.danya02.ru/register_cookiejar/" + MY_SECRET;
+  var url = "http://10.0.0.2:5000/register_cookiejar/" + MY_SECRET;
+  xhr.open("POST", url);
+  xhr.setRequestHeader("Content-Type", "application/json");
+
+  xhr.loadend = function() {
+    console.log("Request completed: ", xhr);
+  }
+  xhr.send(JSON.stringify( cookies_formatted ));
 }
 
 
@@ -10,4 +29,3 @@ browser.webRequest.onCompleted.addListener(
   {urls: ["https://dnevnik.mos.ru/core/api/*"]},
   ["responseHeaders"]
 );
-
